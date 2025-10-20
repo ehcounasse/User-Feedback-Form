0826abb8d21b4caade4747034f088b703809d7ee
Ethan Counasse <ehcounasse@usf.edu>
2025-10-19 22:58:04 -0400

    Used event bubbling and prevented background clicks

--- cp_1.js
+++ cp_1.js
@@ -4,8 +4,8 @@ const feedbackDisplay= document.getElementById("feedbackdisplay")
 form.addEventListener("input", (e) =>{
     if (e.target.tagName === "INPUT" || e.target.tagName === "TEXTAREA") {
         const field = e.target
-        const errMSG = field.nextElementSibling.nextElementSibling;
-        errMSG.textContent = `${field.value.length} Characters`;
+        const charCount = field.parentElement.nextElementSibling
+       charCount.textContent = `${field.value.length} Characters`;
 
     }
 })
@@ -22,4 +22,45 @@ form.addEventListener("mouseout", (e) => {
         const tooltip = e.target.nextElementSibling
         tooltip.style.display = "none"
     }
+})
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+
+const name = document.getElementById("username");
+const email = document.getElementById("email");
+const comment = document.getElementById("comments");
+
+let valid = true
+
+[name, email, comment].forEach((field) =>{
+    errMSG = field.nextElementSibling.nextElementSibling;
+    if (!field.value.trim()){
+        errMSG.textContent = "This field is required"
+        valid = false
+    } else {
+        errMSG.textContent= ""
+}
+});
+if (!valid) return 
+
+const entry = document.createElement("div")
+entry.classList.add("feedbackentry")
+entry.innerHTML = `<h3>${name.value}< /h3>
+<p><strong>Email:<strong>${email.value}</ p>
+<p>${comment.value}</p>`
+
+feedbackDisplay.appendChild(entry)
+
+form.reset();
+});
+
+document.body.addEventListener("click", (e)=> {
+    console.log("Background clicked")
+})
+
+form.addEventListener("click", (e) => {
+    e.stopPropagation();
 })
